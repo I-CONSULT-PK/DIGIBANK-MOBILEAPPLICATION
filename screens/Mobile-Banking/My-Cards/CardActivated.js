4eceb254bed42b53b0a1b1bb29170df8975312c6
Mohammad Humza <mhamza@iconsult.com.pk>
2024-08-12 11:18:58 +0500

    update

--- screens/Mobile-Banking/My-Cards/CardActivated.js
+++ screens/Mobile-Banking/My-Cards/CardActivated.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import CardActivatedDoneBlue from "../../../assets/CardActivatedDoneBlue.png"; // Ensure this path is correct
+import CardActivatedDoneBlue from "../../../assets/CardActivatedDoneBlue.png"; 
 import CustomButton from "../../../components/Button";
 
 const CardActivated = () => {
@@ -12,13 +12,11 @@ const CardActivated = () => {
       <View className="p-6 items-center">
         <Image source={CardActivatedDoneBlue} className="w-26 h-26 mb-4" />
         <Text className="text-2xl font-bold">Congratulations 🎉</Text>
-        <Text className="text-base  text-gray-500 mt-2 text-center">
-          Lorem ipsum dolor sit amet consectetur. Ornare lorem velit ultrices
-          blandit
-        </Text>
+       
         <Text className="text-base  text-gray-500 mt-10 text-center">
-          Lorem ipsum dolor sit amet consectetur. Ornare lorem velit ultrices
-          blandit sit amet consectetur. Ornare lorem velit ultrices blandit
+          Your card is now active and ready to use. Enjoy seamless transactions,
+          manage your finances, and access exclusive offers with Digi Bank.
+          Happy banking!
         </Text>
         {/* <View className="p-5">
           <CustomButton
